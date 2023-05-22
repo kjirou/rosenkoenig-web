dba8991f6d422e6c233179012675e9cc276cb8bd
kjirou <kjirou.web@gmail.com>
2023-05-22 09:07:23 +0900

    Move jest configs to jest.config.json

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    // Ignore the match targets from the transpile.
+    // And because of the "?!" so the packages mentioned here are transpile targets.
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+  ]
+};
+
+module.exports = config;
